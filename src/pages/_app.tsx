71a04bbd9e5a895f0ed603961c2c08f25bd2c738
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-05-03 11:46:26 -0300

    global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
-function MyApp({ Component, pageProps }) {
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  globalStyles();
+
   return <Component {...pageProps} />
 }
-
-export default MyApp
