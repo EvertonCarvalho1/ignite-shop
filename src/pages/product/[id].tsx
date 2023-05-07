fcdaff03ff824b888ea0aa211288c0bb5e975e10
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-05-07 15:32:23 -0300

    product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,34 @@
-import { useRouter } from "next/router"
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
+import Image from "next/image";
 
-export default function Product() {
+export default function Home() {
     const { query } = useRouter();
 
     return (
-        <h1>Product {JSON.stringify(query)}</h1>
+        <>
+            <Head>
+                <title> | Ignite Shop</title>
+            </Head>
+
+            <ProductContainer>
+                <ImageContainer>
+                    {/* <Image src='https://avatars.githubusercontent.com/u/82480230?v=4' width={520} height={480} alt="" /> */}
+                </ImageContainer>
+
+                <ProductDetails>
+                    <h1>Camiseta</h1>
+                    <span>79</span>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quidem, porro natus officiis omnis doloribus commodi. Voluptatibus tempore dignissimos officiis? Corporis, culpa et. Ex adipisci corrupti animi! Quia, libero accusamus!</p>
+
+                    <button>
+                        Comprar agora
+                    </button>
+                </ProductDetails>
+            </ProductContainer>
+        </>
     )
 }
+
