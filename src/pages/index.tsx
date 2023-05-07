726ed19de7e0d8d8eb87f6c07924f14bbb85fddd
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-05-06 23:20:44 -0300

    using SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import { useKeenSlider } from 'keen-slider/react';
-
-import camiseta1 from '../assets/camisetas/1.png';
-import camiseta2 from '../assets/camisetas/2.png';
-import camiseta3 from '../assets/camisetas/3.png';
 
 import 'keen-slider/keen-slider.min.css';
 import { stripe } from "../lib/stripe";
@@ -47,7 +43,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   });
@@ -66,6 +62,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products: products
-    }
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
